b91bd21aa78b580e4717744e946c1e179994804d
Morning <morning@mstars-lab2-14s-iMac.local>
2023-01-16 11:34:16 +0800

    Router added about login register form and home page also not found 404 page

--- day-43/src/App.js
+++ day-43/src/App.js
@@ -6,16 +6,36 @@ import Login from './component/Login';
 import About from './component/About';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './component/Register';
+import Home from './component/Home';
+import Usukhuu from './component/about/usukhuu';
+import Khangai from './component/about/khangai';
+import NotFound from './component/NotFound';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Day 43-React router</h1>
+      <div className='navbar'>
+        <a href='./about'>About</a>
+        <a href='./login'>login</a>
+        <a href='./register'>register</a>
+        <a href='./form'>Form</a>
+        <a href='./'>home</a>
+      </div>
       <Routes>
-        <Route path='/about' element={<About />}/>
+        <Route path='/about/*' element={<About />}>
+          <Route path='usukhuu' element={<Usukhuu />}/>
+          <Route path='khangai' element={<Khangai />}/>
+        </Route>
+
+
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
+        <Route path='/form' element={<Form />}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='*' element={<NotFound />}/>
+        
       </Routes>
     </div>
   );
