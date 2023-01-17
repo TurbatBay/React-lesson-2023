f592cfb513a94e466ec4842dff56c7b39c211f7b
Morning <morning@mstars-lab2-14s-iMac.local>
2023-01-17 11:45:00 +0800

    changed props in movie detailed page

--- day-44/src/component/Movie.jsx
+++ day-44/src/component/Movie.jsx
@@ -1,16 +1,27 @@
 import { useLocation, useParams } from "react-router-dom"
+import movieData from "../data/movies";
 
-export default function Movie (){
-    const {id} = useParams()
+export default function Movie() {
+    const { id } = useParams()
     const location = useLocation();
     console.log(location.state)
-    return(
+    const movie = movieData.filter((data) => data.id == id)
+    return (
         <div>
             <h1>Movie detailed page {id}</h1>
-            <div>{location.state.id}</div>
-            <div>{location.state.name}</div>
-            <div>{location.state.ISBN}</div>
-            
+            {movie &&
+                movie.map((m, idx) => {
+                    return (
+
+                        <div key={idx}>
+                            <p>{m.name}</p>
+                            <p>{m.id}</p>
+                            <p>{m.ISBN}</p>
+                        </div>
+                    )
+
+
+                })}
         </div>
     )
 }
