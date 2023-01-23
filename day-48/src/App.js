c3ec30a556170c14557e7306df9f9cb2f0f975c4
Morning <morning@mstars-lab2-14s-iMac.local>
2023-01-23 10:26:58 +0800

    Anime API awaad teriigaa useEffect ashiglaad Next Page button darah uyd page n soligdoh ym hiilee

--- day-48/src/App.js
+++ day-48/src/App.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+import Anime from "./components/Anime";
 import Input from "./components/Input";
+import Button from "./components/Button";
+import { Tooltip } from "react-bootstrap";
+import TopAnime from "./components/TopAnime";
 function App() {
   const [form, setForm] = useState("");
 
@@ -10,8 +14,11 @@ function App() {
   }
   return (
     <div className="App">
-      <Input label="First" value={form} onChange={handleInput} />
-      <Input label="Second" value={form} onChange={handleInput} />
+      {/* <Input label="First" value={form} onChange={handleInput} />
+      <Input label="Second" value={form} onChange={handleInput} /> */}
+      {/* <Button />
+      <Anime /> */}
+      <TopAnime />
     </div>
   );
 }
