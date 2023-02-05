3ecddc0999c4e782c1d69f03cf21ec6cae22f077
TurbatBay <learnwithturu@gmail.com>
2023-02-05 09:34:00 +0800

    adding new branch

--- admin-panel-project/fe/src/pages/HomePage.jsx
+++ admin-panel-project/fe/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
       <Typography variant="h2" color="white">
-        Homepage
+        Homepage testing branch
       </Typography>
       <UsersTable />
     </div>
