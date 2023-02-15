d756ad7cbad5e54006b562cdcbb99089ac7026a2
Morning <morning@mstars-lab2-14s-iMac.local>
2023-02-15 12:30:17 +0800

    category too table deeree maplah gj oroldloo ghde aldaatai

--- day-63-product-category-crud/fe/src/components/Categories.jsx
+++ day-63-product-category-crud/fe/src/components/Categories.jsx
@@ -28,16 +28,21 @@ const Categories = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Test Category</td>
-            <td>
-              <button>Edit</button>
-            </td>
-            <td>
-              <button>Delete</button>
-            </td>
-          </tr>
+          {categories &&
+            categories.map((category, index) => {
+              return (
+                <tr>
+                  <td>{category.id}</td>
+                  <td>{category.name}</td>
+                  <td>
+                    <button>Edit</button>
+                  </td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              )
+            })}
         </tbody>
       </table>
     </div>
