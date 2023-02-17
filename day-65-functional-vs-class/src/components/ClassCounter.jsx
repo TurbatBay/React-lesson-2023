85726da50f052e1444551511d4a640ca97b15e76
Morning <morning@mstars-lab2-14s-iMac.local>
2023-02-17 10:10:08 +0800

    Adding day 65 first half

---
+++ day-65-functional-vs-class/src/components/ClassCounter.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+class ClassCounter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+    this.increase = this.increase.bind(this)
+  }
+
+  increase() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  componentDidMount() {
+    console.log('component mounted')
+  }
+  componentWillUnMount() {
+    console.log('component unmounted')
+  }
+
+  render() {
+    return (
+      <div style={{ margin: '50px' }}>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increase}>Add</button>
+      </div>
+    )
+  }
+}
+
+export { ClassCounter }
