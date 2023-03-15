d4d88697e59e5c9a710f834e01154d747b291730
Morning <morning@mstars-lab2-14s-iMac.local>
2023-03-15 12:30:20 +0800

    day 77 mongoose

---
+++ day-77-mongoose-setup/backend/index.js
@@ -0,0 +1,25 @@
+console.log('Day - 77 - Mongoose setup')
+
+const { application, Router } = require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const apiRouter = require('./routes/api')
+const PORT = 8080
+const MONGO_CONNECTION_STRING =
+  'mongodb+srv://MongoTuru:7IKP4mQadrfYFsa8@turumongo.pfftwk3.mongodb.net/test' //ene cloud deer haana bairlahiig zaaj ugnu (test dotor mini data orson bga)
+const app = express()
+
+app.use(express.json())
+app.use(apiRouter)
+mongoose
+  .connect(MONGO_CONNECTION_STRING)
+  .then(() => console.log('Database connected successfully'))
+  .catch((error) => console.error(error))
+
+app.get('/', (request, response) => {
+  response.send('<h1> DAY - 77 - Mongoose App </h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express Application is running on http://localhost:${PORT}`)
+})
