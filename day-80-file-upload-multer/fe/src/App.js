5b557fa68d1ae707e7b0e8fdaa3dc47d0849e007
TurbatBay <learnwithturu@gmail.com>
2023-03-20 12:24:29 +0800

    march-20 File upload Multer

---
+++ day-80-file-upload-multer/fe/src/App.js
@@ -0,0 +1,12 @@
+import FileUpload from './components/FileUpload'
+import './App.css'
+function App() {
+  return (
+    <div className='App'>
+      <h1>Day 80 File Upload Multer</h1>
+      <FileUpload />
+    </div>
+  )
+}
+
+export default App
