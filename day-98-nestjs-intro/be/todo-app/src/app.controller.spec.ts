d88483fa6561f198d749a951455e65e22e51cb8f
TurbatBay <learnwithturu@gmail.com>
2023-04-17 09:36:23 +0800

    NestJS intro

---
+++ day-98-nestjs-intro/be/todo-app/src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Day-98 NestJS intro!"', () => {
+      expect(appController.getHello()).toBe('Day-98 NestJS intro!');
+    });
+  });
+});
